509f89a3d9c2510a4c41b274f539c3f47965484b
syota-keica <kondo@keica.co.jp>
2021-11-15 23:17:39 +0900

    TaskをAPI経由で登録

--- resources/js/components/TaskCreate.vue
+++ resources/js/components/TaskCreate.vue
@@ -2,18 +2,18 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-sm-6">
-                <form>
+                 <form @submit.prevent="submit">
                     <div class="form-group row">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
-                        <input type="text" class="col-sm-9 form-control" id="title">
+                        <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title">
                     </div>
                     <div class="form-group row">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
-                        <input type="text" class="col-sm-9 form-control" id="content">
+                        <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content">
                     </div>
                     <div class="form-group row">
                         <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
-                        <input type="text" class="col-sm-9 form-control" id="person-in-charge">
+                        <input type="text" class="col-sm-9 form-control" id="person-in-charge" v-model="task.person_in_charge">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
@@ -23,5 +23,24 @@
 </template>
 
 <script>
-    export default {}
+import {ref, onMounted} from 'vue';
+import {useRouter} from 'vue-router';
+    export default {
+        setup(){
+            const task = {};
+            const router = useRouter()
+
+            const submit = () => {
+                axios.post('/api/tasks', task)
+                .then((res) => {
+                    router.push({name: 'task.list'});
+                })
+            }
+
+            return {
+                submit,
+                task,
+            }
+        }
+    }
 </script>
